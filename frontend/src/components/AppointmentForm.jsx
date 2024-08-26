96982917b0a4b110cc9c8ad0d15ece1dec869010
Lakshya845 <lakshaychaudhary147@gmail.com>
2024-08-26 16:20:28 +0530

    Update AppointmentForm.jsx

--- frontend/src/components/AppointmentForm.jsx
+++ frontend/src/components/AppointmentForm.jsx
@@ -62,7 +62,7 @@ const AppointmentForm = () => {
   useEffect(() => {
     const fetchDoctors = async () => {
       const { data } = await axios.get(
-        "http://localhost:4000/api/v1/user/doctors",
+        "https://healthcare-management-backend.onrender.com",
         { withCredentials: true }
       );
       setDoctors(data.doctors);
@@ -75,7 +75,7 @@ const AppointmentForm = () => {
     try {
       const hasVisitedBool = Boolean(hasVisited);
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/appointment/post",
+        "https://healthcare-management-backend.onrender.com",
         {
           firstName,
           lastName,
