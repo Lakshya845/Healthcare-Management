5d094670e6897c02a276eb8502ba4c41fc7efa7e
Lakshya845 <lakshaychaudhary147@gmail.com>
2024-08-29 14:13:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 import app from "./app.js";
 import cloudinary from "cloudinary";
-const cors = require('cors')
-app.use(cors());
+
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
